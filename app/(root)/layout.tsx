1266db8e7cc59e9db15401724c6e3fe476871125
WOW ROOMS <wowrooms@WOWs-MacBook-Pro.local>
2024-06-21 23:09:07 +0530

    added some sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
+import SideBar from '@/components/share/SideBar'
 import React from 'react'
 
 function Layout({children}: {children: React.ReactNode}) {
   return (
     <main className='root'>
+      <SideBar />
         <div className='root-container'>
           <div className='wraper'>
                {children}
